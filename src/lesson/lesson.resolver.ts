e011376c322a5aec855c8a4709f272d374385466
dulait <dd20211096@student.fon.bg.ac.rs>
2024-03-14 09:16:57 +0100

    basic queries & mutation

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -1,17 +1,30 @@
-import { Query, Resolver } from "@nestjs/graphql";
+import { Args, Mutation, Query, Resolver } from "@nestjs/graphql";
 import { LessonType } from "./lesson.type";
+import { LessonService } from "./lesson.service";
+import { CreateLessonInput } from "./lesson.input";
 
 @Resolver(of => LessonType)
 export class LessonResolver {
 
+    constructor(private lessonService: LessonService) { }
+
     @Query(returns => LessonType)
-    lesson() {
-        return {
-            id: 'asdasd',
-            name: 'Math 1',
-            startDate: (new Date()).toISOString(),
-            endDate: (new Date()).toISOString(),
-        }
+    getLesson(
+        @Args('id') id: string,
+    ) {
+        return this.lessonService.getLesson(id);
+    }
+
+    @Query(returns => [LessonType])
+    getAllLessons() {
+        return this.lessonService.getAllLessons();
+    }
+
+    @Mutation(returns => LessonType)
+    createLesson(
+        @Args('createLessonInput') createLessonInput: CreateLessonInput,
+    ) {
+        return this.lessonService.createLesson(createLessonInput);
     }
 
 }
