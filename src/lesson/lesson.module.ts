b293163d07420b4d94141226d9fef1f1be54e6a7
dulait <dd20211096@student.fon.bg.ac.rs>
2024-03-14 10:12:54 +0100

    import student module

--- src/lesson/lesson.module.ts
+++ src/lesson/lesson.module.ts
@@ -3,16 +3,18 @@ import { Module } from '@nestjs/common';
 import { LessonResolver } from './lesson.resolver';
 import { LessonService } from './lesson.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { StudentModule } from 'src/student/student.module';
 
 @Module({
     imports: [
         TypeOrmModule.forFeature([
             Lesson,
-        ])
+        ]),
+        StudentModule
     ],
     providers: [
         LessonResolver,
-        LessonService
+        LessonService,
     ],
 })
 export class LessonModule { }
