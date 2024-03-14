28d0b36dec649255aa5ec61b89e25a5d401560ee
dulait <dd20211096@student.fon.bg.ac.rs>
2024-03-14 09:16:40 +0100

    add imports & providers

--- src/lesson/lesson.module.ts
+++ src/lesson/lesson.module.ts
@@ -1,8 +1,18 @@
+import { Lesson } from './lesson.entity';
 import { Module } from '@nestjs/common';
 import { LessonResolver } from './lesson.resolver';
 import { LessonService } from './lesson.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-    providers: [LessonResolver, LessonService],
+    imports: [
+        TypeOrmModule.forFeature([
+            Lesson,
+        ])
+    ],
+    providers: [
+        LessonResolver,
+        LessonService
+    ],
 })
 export class LessonModule { }
