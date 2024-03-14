b4dea715b23c498b8f8a5323f58cf3fe4b923454
dulait <dd20211096@student.fon.bg.ac.rs>
2024-03-14 10:12:45 +0100

    create dto for adding students to a lesson

---
+++ src/lesson/assign-students-to-lesson.input.ts
@@ -0,0 +1,15 @@
+import { Field, ID, InputType } from "@nestjs/graphql";
+import { IsUUID } from "class-validator";
+
+@InputType()
+export class AssignStudentsToLessonInput {
+
+    @IsUUID()
+    @Field(() => ID)
+    lessonId: string;
+
+    @IsUUID("4", { each: true })
+    @Field(() => [ID])
+    studentIds: string[]
+
+}
